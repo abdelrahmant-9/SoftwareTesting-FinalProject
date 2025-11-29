35bcefe02d308eb2daf16d56902196477aa4e5ac
Abdelrahman <abdot9674@gmail.com>
2025-11-29 23:40:49 +0200

    add import

--- src/main/java/integration/Checkout/Integration_Testing.java
+++ src/main/java/integration/Checkout/Integration_Testing.java
@@ -393,7 +393,7 @@ public class Integration_Testing {
 
     @AfterMethod
     public void closeBrowser() throws InterruptedException {
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         driver.quit();
     }
 }
--- src/main/java/integration/Login/Integration_Testcases.java
+++ src/main/java/integration/Login/Integration_Testcases.java
@@ -14,6 +14,7 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Integration_Testcases {
