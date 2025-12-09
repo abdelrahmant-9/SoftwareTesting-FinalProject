499fc0c89fd2ca1da5b52c32a535590cbe3cee58
Abdelrahman <abdot9674@gmail.com>
2025-12-09 16:24:21 +0200

    Edit unit cart file and remove test-output from tracking

--- src/main/java/Unit/RemainingPages/Cart.java
+++ src/main/java/Unit/RemainingPages/Cart.java
@@ -13,141 +13,137 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Cart {
+
     WebDriver driver;
 
     @BeforeClass
     public void setup() {
         ChromeOptions options = new ChromeOptions();
-        Map<String, Object> prefs = new HashMap();
+        Map<String, Object> prefs = new HashMap<>();
         prefs.put("credentials_enable_service", false);
         prefs.put("profile.password_manager_enabled", false);
         prefs.put("profile.password_manager_leak_detection", false);
+        prefs.put("profile.credentials_enable_autosignin", false);
+        prefs.put("password_manager_enabled", false);
         options.setExperimentalOption("prefs", prefs);
-        options.addArguments(new String[]{"--disable-save-password-bubble"});
-        options.addArguments(new String[]{"--disable-notifications"});
-        this.driver = new ChromeDriver(options);
-        this.driver.manage().window().maximize();
-        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3L));
-        this.driver.navigate().to("https://www.saucedemo.com/");
+        options.addArguments("--disable-features=PasswordManagerEnabled");
+        options.addArguments("--disable-features=PasswordLeakDetection");
+        options.addArguments("--disable-features=SafeBrowsingSecurityToken");
+        options.addArguments("--disable-features=SafetyTipUI");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.saucedemo.com/");
     }
 
-    @Test(
-            priority = 1
-    )
+    @Test(priority = 1)
     public void Login() throws InterruptedException {
-        this.slowSendKeys(By.cssSelector("[data-test='username']"), "standard_user");
-        this.slowSendKeys(By.cssSelector("[data-test='password']"), "secret_sauce");
-        this.slowClick(By.cssSelector("[data-test='login-button']"));
-        this.waitSlow();
-        Assert.assertTrue(this.driver.getCurrentUrl().contains("inventory.html"));
+        sendKeysWithDelay(By.cssSelector("[data-test='username']"), "standard_user");
+        sendKeysWithDelay(By.cssSelector("[data-test='password']"), "secret_sauce");
+        clickWithDelay(By.cssSelector("[data-test='login-button']"));
+        shortPause();
+        Assert.assertTrue(driver.getCurrentUrl().contains("inventory.html"));
     }
 
-    @Test(
-            priority = 2
-    )
+    @Test(priority = 2)
     public void CartBadgeCounter() throws InterruptedException {
-        this.slowClick(By.cssSelector("[data-test='add-to-cart-sauce-labs-backpack']"));
-        this.slowClick(By.cssSelector("[data-test='add-to-cart-sauce-labs-bike-light']"));
-        this.slowClick(By.cssSelector("[data-test='add-to-cart-sauce-labs-bolt-t-shirt']"));
-        this.waitSlow();
-        String badge = this.driver.findElement(By.cssSelector("[data-test='shopping-cart-badge']")).getText();
+        clickWithDelay(By.cssSelector("[data-test='add-to-cart-sauce-labs-backpack']"));
+        clickWithDelay(By.cssSelector("[data-test='add-to-cart-sauce-labs-bike-light']"));
+        clickWithDelay(By.cssSelector("[data-test='add-to-cart-sauce-labs-bolt-t-shirt']"));
+        shortPause();
+
+        String badge = driver.findElement(By.cssSelector("[data-test='shopping-cart-badge']")).getText();
         Assert.assertEquals(badge, "3");
-        this.waitSlow();
-        this.slowClick(By.cssSelector("[data-test='remove-sauce-labs-bike-light']"));
-        this.slowClick(By.cssSelector("[data-test='remove-sauce-labs-bolt-t-shirt']"));
-        this.waitSlow();
-        badge = this.driver.findElement(By.cssSelector("[data-test='shopping-cart-badge']")).getText();
+
+        shortPause();
+        clickWithDelay(By.cssSelector("[data-test='remove-sauce-labs-bike-light']"));
+        clickWithDelay(By.cssSelector("[data-test='remove-sauce-labs-bolt-t-shirt']"));
+        shortPause();
+
+        badge = driver.findElement(By.cssSelector("[data-test='shopping-cart-badge']")).getText();
         Assert.assertEquals(badge, "1");
-        this.waitSlow();
+        shortPause();
     }
 
-    @Test(
-            priority = 6
-    )
+    @Test(priority = 6)
     public void OpenCart() throws InterruptedException {
-        this.slowClick(By.cssSelector("[data-test='shopping-cart-link']"));
-        Assert.assertTrue(this.driver.getCurrentUrl().contains("cart.html"));
-        this.waitSlow();
+        clickWithDelay(By.cssSelector("[data-test='shopping-cart-link']"));
+        Assert.assertTrue(driver.getCurrentUrl().contains("cart.html"));
+        shortPause();
     }
 
-    @Test(
-            priority = 7
-    )
+    @Test(priority = 7)
     public void ButtonsInCart() {
-        Assert.assertTrue(this.driver.findElement(By.cssSelector("[data-test='remove-sauce-labs-backpack']")).isDisplayed());
-        Assert.assertTrue(this.driver.findElement(By.cssSelector("[data-test='continue-shopping']")).isDisplayed());
-        Assert.assertTrue(this.driver.findElement(By.cssSelector("[data-test='checkout']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("[data-test='remove-sauce-labs-backpack']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("[data-test='continue-shopping']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("[data-test='checkout']")).isDisplayed());
     }
 
-    @Test(
-            priority = 8
-    )
+    @Test(priority = 8)
     public void ContinueShopping() throws InterruptedException {
-        this.slowClick(By.cssSelector("[data-test='continue-shopping']"));
-        Assert.assertTrue(this.driver.getCurrentUrl().contains("inventory.html"));
-        this.waitSlow();
-        this.slowClick(By.cssSelector("[data-test='shopping-cart-link']"));
-        this.waitSlow();
+        clickWithDelay(By.cssSelector("[data-test='continue-shopping']"));
+        Assert.assertTrue(driver.getCurrentUrl().contains("inventory.html"));
+        shortPause();
+
+        clickWithDelay(By.cssSelector("[data-test='shopping-cart-link']"));
+        shortPause();
     }
 
-    @Test(
-            priority = 9
-    )
+    @Test(priority = 9)
     public void Checkout() throws InterruptedException {
-        this.slowClick(By.cssSelector("[data-test='checkout']"));
-        this.waitSlow();
-        Assert.assertTrue(this.driver.findElement(By.cssSelector("[data-test='cancel']")).isDisplayed());
-        this.slowClick(By.cssSelector("[data-test='cancel']"));
-        Assert.assertTrue(this.driver.getCurrentUrl().contains("cart.html"));
-        this.waitSlow();
+        clickWithDelay(By.cssSelector("[data-test='checkout']"));
+        shortPause();
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("[data-test='cancel']")).isDisplayed());
+        clickWithDelay(By.cssSelector("[data-test='cancel']"));
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("cart.html"));
+        shortPause();
     }
 
-    @Test(
-            priority = 10
-    )
+    @Test(priority = 10)
     public void ProductDescription() throws InterruptedException {
-        this.slowClick(By.cssSelector("[data-test='inventory-item-name']"));
-        this.waitSlow();
-        Assert.assertTrue(this.driver.findElement(By.cssSelector("[data-test='back-to-products']")).isDisplayed());
-        Assert.assertTrue(this.driver.findElement(By.cssSelector("[data-test='remove']")).isDisplayed());
-        this.slowClick(By.cssSelector("[data-test='back-to-products']"));
-        this.waitSlow();
+        clickWithDelay(By.cssSelector("[data-test='inventory-item-name']"));
+        shortPause();
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("[data-test='back-to-products']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("[data-test='remove']")).isDisplayed());
+
+        clickWithDelay(By.cssSelector("[data-test='back-to-products']"));
+        shortPause();
     }
 
-    @Test(
-            priority = 11
-    )
+    @Test(priority = 11)
     public void RemoveProduct() throws InterruptedException {
-        this.slowClick(By.cssSelector("[data-test='shopping-cart-link']"));
-        this.waitSlow();
-        this.slowClick(By.cssSelector("[data-test='remove-sauce-labs-backpack']"));
-        this.waitSlow();
-        boolean empty = this.driver.findElements(By.className("cart_item")).isEmpty();
+        clickWithDelay(By.cssSelector("[data-test='shopping-cart-link']"));
+        shortPause();
+
+        clickWithDelay(By.cssSelector("[data-test='remove-sauce-labs-backpack']"));
+        shortPause();
+
+        boolean empty = driver.findElements(By.className("cart_item")).isEmpty();
         Assert.assertTrue(empty);
     }
 
     @AfterClass
     public void closeBrowser() throws InterruptedException {
-        Thread.sleep(5000L);
-        this.driver.quit();
+        Thread.sleep(2000);
+        driver.quit();
     }
 
-    public void slowClick(By locator) throws InterruptedException {
-        this.driver.findElement(locator).click();
-        Thread.sleep(1200L);
+    public void clickWithDelay(By locator) throws InterruptedException {
+        driver.findElement(locator).click();
+        Thread.sleep(500);
     }
 
-    public void slowSendKeys(By locator, String text) throws InterruptedException {
-        for(char c : text.toCharArray()) {
-            this.driver.findElement(locator).sendKeys(new CharSequence[]{String.valueOf(c)});
-            Thread.sleep(120L);
+    public void sendKeysWithDelay(By locator, String text) throws InterruptedException {
+        for (char c : text.toCharArray()) {
+            driver.findElement(locator).sendKeys(String.valueOf(c));
+            Thread.sleep(50);
         }
-
     }
 
-    public void waitSlow() throws InterruptedException {
-        Thread.sleep(2000L);
+    public void shortPause() throws InterruptedException {
+        Thread.sleep(5000);
     }
 }
-
-
